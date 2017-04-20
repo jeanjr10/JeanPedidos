8b95a987e71131e862036dfcd9855c2df07f3db6
Jean Jr <jeanjorgejr14g@gmail.com>
2017-04-20 16:31:37 -0300

    Alteracoes

--- src/main/java/org/JeanPedido/mb/CadastraProdutoBean.java
+++ src/main/java/org/JeanPedido/mb/CadastraProdutoBean.java
@@ -6,6 +6,8 @@
 package org.JeanPedido.mb;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.JeanPedidos.DAO.ProdutoDAO;
